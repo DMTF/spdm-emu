5acab524305815c8e671c9c420a73561c2b9d86c
Jiewen Yao <jiewen.yao@intel.com>
2022-04-01 22:32:51 +0800

    Update copyright.
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- spdm_emu/spdm_responder_emu/spdm_responder_session.c
+++ spdm_emu/spdm_responder_emu/spdm_responder_session.c
@@ -1,6 +1,6 @@
 /**
  *  Copyright Notice:
- *  Copyright 2021 DMTF. All rights reserved.
+ *  Copyright 2021-2022 DMTF. All rights reserved.
  *  License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/spdm-emu/blob/main/LICENSE.md
  **/
 
