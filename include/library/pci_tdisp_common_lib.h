d17587b829f87ab885f82b079cd41ba9ccc3748d
Gustavo Romero <gustavo.romero@linaro.org>
2026-06-07 00:55:28 +0000

    Add LIBTDISP_MAX_MESSAGE_SIZE definition
    Add a definition for the maximum size of a TDISP message.
    
    Signed-off-by: Gustavo Romero <gustavo.romero@linaro.org>

--- include/library/pci_tdisp_common_lib.h
+++ include/library/pci_tdisp_common_lib.h
@@ -16,4 +16,14 @@
 
 #define LIBTDISP_ERROR_MESSAGE_MAX_SIZE (sizeof(pci_tdisp_error_response_t))
 
+/*
+ * Maximum size of a TDISP response message: the largest response is
+ * GET_DEVICE_INTERFACE_REPORT, which carries up to
+ * LIBTDISP_INTERFACE_REPORT_MAX_SIZE bytes of report data following the
+ * report-response header.
+ */
+#define LIBTDISP_MAX_MESSAGE_SIZE \
+    (sizeof(pci_tdisp_device_interface_report_response_t) + \
+     LIBTDISP_INTERFACE_REPORT_MAX_SIZE)
+
 #endif
